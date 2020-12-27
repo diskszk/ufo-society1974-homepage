3e541ed8a2930491b72c4c93a460e285cc84d9d1
diskszk <ibalucky23@gmail.com>
2020-12-27 20:14:36 +0900

    add: add routing

--- public/src/index.tsx
+++ public/src/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { Switch, Route, HashRouter } from "react-router-dom";
+
+// PageComponents
+import { App, Page, UndefinedPage } from "./pages";
 
 render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <Switch>
+        <Route exact path={"/"} component={App} />
+        <Route exact path={"/page"} component={Page} />
+        <Route component={UndefinedPage} />
+      </Switch>
+    </HashRouter>
   </Provider>,
   document.getElementById("app")
 );
