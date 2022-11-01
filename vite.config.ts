a287677fbd75f42fec77b7092b93cff067c43eb7
diskszk <ibalucky23@gmail.com>
2022-11-01 22:41:15 +0900

    fix: viteでreactを動かすためにhtmlファイルをルートに移動する

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { resolve } from "path";
 const PUBLIC_PATH = resolve(__dirname, "public");
 
 export default defineConfig({
-  root: PUBLIC_PATH,
   plugins: [react(), eslint()],
   server: {
     host: true,
@@ -17,12 +16,12 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(PUBLIC_PATH, "/index.html"),
-        404: resolve(PUBLIC_PATH, "/404.html"),
-        albums: resolve(PUBLIC_PATH, "/albums.html"),
-        diary: resolve(PUBLIC_PATH, "/diary.html"),
-        jikosyoukai: resolve(PUBLIC_PATH, "/jikosyoukai.html"),
-        link: resolve(PUBLIC_PATH, "/link.html"),
+        main: resolve(__dirname, "/index.html"),
+        404: resolve(__dirname, "/404.html"),
+        albums: resolve(__dirname, "/albums.html"),
+        diary: resolve(__dirname, "/diary.html"),
+        jikosyoukai: resolve(__dirname, "/jikosyoukai.html"),
+        link: resolve(__dirname, "/link.html"),
       },
     },
   },
