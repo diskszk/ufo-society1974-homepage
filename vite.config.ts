ffd3477d597c939858cb20267a1b342bf5e242c4
diskszk <ibalucky23@gmail.com>
2022-11-02 18:50:32 +0900

    feat: vitestを導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
@@ -17,6 +19,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+  },
+
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    root: "./src/",
+    setupFiles: "./vitest.setup.ts",
   },
 
   build: {
